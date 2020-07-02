a5b9f55ff6be1d982294f0d746cc50d4e050ad3f
begnocjp <62443976+begnocjp@users.noreply.github.com>
2020-07-02 10:31:44 -0500

    update

--- patrick_Alerting_nocue.m
+++ patrick_Alerting_nocue.m
@@ -16,7 +16,7 @@ fprintf('%s %s %s \n','----- Begin Trial Definition -----','Participant:',wpms.n
 
 
 
-value1      = [find(strcmp('wnou', {cfg.event.value}))]'%compare for both 'wnou' and 'wnod' by comparing only first two letter
+value1      = [find(strcmp('wnou', {cfg.event.value}))]'% find trials with trigger value 'wnou'
 value2      = [find(strcmp('wnod', {cfg.event.value}))]'
 value       = vertcat(value1, value2) %combine the two
 
@@ -24,6 +24,12 @@ value       = vertcat(value1, value2) %combine the two
 sample1     = [cfg.event(find(strcmp('wnou', {cfg.event.value}))).sample]'; %get samples for both conditions
 sample2     = [cfg.event(find(strcmp('wnod', {cfg.event.value}))).sample]';
 sample =       vertcat(sample1, sample2)  %combine the two
+
+
+info = cfg.event(value) %get trial info for both conditions
+info(:).value
+
+
 
 
 % determine the number of samples before and after the trigger
@@ -40,27 +46,14 @@ cfg.trl = [trlbegin trlend offset]; % concatenate the columns into the trl matri
   
   
  trdat = ft_redefinetrial(cfg,data);
-    trdat.trialinfo  = trdat.trialinfo(:,1);
-    %save([wpms.dirs.CWD wpms.dirs.preproc wpms.names{name_i} '_EOGCORR_trdat'],'trdat','-v7.3'); % added this into to save trial data for auto artifact rejeciton 
+ 
+ info = cfg.event(value) %get trial info for both conditions
+
+  
+    trdat.trialinfo  =  {info(:).value}';
+    save([wpms.dirs.CWD wpms.dirs.preproc wpms.names{name_i} '_EOGCORR_trdat'],'trdat','-v7.3'); % added this into to save trial data for auto artifact rejeciton 
     %clear data tdat%tidying
 
-    
-
-
-
-% % look for the combination of a trigger "7" followed by a trigger "64"
-% % for each trigger except the last one
-% trl = [];
-% for j = 1:(length(value)-1)
-% trg1 = value(j);
-% trg2 = value(j+1);
-% if trg1==7 && trg2==64
-%   trlbegin = sample(j) + pretrig;
-%   trlend   = sample(j) + posttrig;
-%   offset   = pretrig;
-%   newtrl   = [trlbegin trlend offset];
-%   trl      = [trl; newtrl];
-% end
 
 
 % WARNING STIM CODES
