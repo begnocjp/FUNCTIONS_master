529f168e4cddfd65335d2b6ef2287a97122b7893
begnocjp <62443976+begnocjp@users.noreply.github.com>
2020-07-07 13:55:26 -0500

    update

--- patrick_Alerting_batch_test.m
+++ patrick_Alerting_batch_test.m
@@ -20,18 +20,17 @@ value1      = [find(strcmp('wnou', {cfg.event.value}))]'% find trials with trigg
 value2      = [find(strcmp('wnod', {cfg.event.value}))]'
 value3      = [find(strcmp('wdou', {cfg.event.value}))]'% find trials with trigger value for "double"
 value4      = [find(strcmp('wdud', {cfg.event.value}))]'
-value_n      = vertcat(value1, value2) %combine the two
-value_d      = vertcat(value3, value4) %combine the two
+value      = vertcat(value1, value2, value3, value4) %combine 
 
 sample1     = [cfg.event(find(strcmp('wnou', {cfg.event.value}))).sample]'; %get samples for both conditions
 sample2     = [cfg.event(find(strcmp('wnod', {cfg.event.value}))).sample]';
 sample3     = [cfg.event(find(strcmp('wdou', {cfg.event.value}))).sample]'; %get samples for both conditions
 sample4     = [cfg.event(find(strcmp('wdud', {cfg.event.value}))).sample]';
-sample_n =       vertcat(sample1, sample2)  %combine the two
-sample_d =       vertcat(sample3, sample4)  %combine the two
+sample      =  vertcat(sample1, sample2, sample3, sample4)  %combine 
 
 
-info.no_cue = cfg.event(value_n) %get trial info for both conditions
+
+info = cfg.event(value) %get trial info for both conditions
 % make trial.info - 1 = first marker "wnou" 2 = second marker "wnod"
 
  value_marker1 = value1
@@ -39,20 +38,14 @@ info.no_cue = cfg.event(value_n) %get trial info for both conditions
  
   value_marker2 = value2
   value_marker2(:,1) = 2
- 
- value_marker_no_cue      = vertcat(value_marker1, value_marker2) 
   
-info.double_cue = cfg.event(value_d) %get trial info for both conditions
-
-% make trial.info - 1 = first marker "wnou" 2 = second marker "wnod"
-
  value_marker3 = value3
  value_marker3(:,1) = 3 
  
   value_marker4 = value4
   value_marker4(:,1) = 4
  
- value_marker_double      = vertcat(value_marker3, value_marker4) 
+ value_marker     = vertcat(value_marker1, value_marker2, value_marker3, value_marker4) 
    
 
 % determine the number of samples before and after the trigger
@@ -61,31 +54,21 @@ posttrig =  round(cfg.trialdef.post * cfg.hdr.Fs);
 
 %make trl matrix no cue
 cfg.trl = []
-trlbegin = sample_n + pretrig;      %  begin trial
-trlend   = sample_n + posttrig;     %  end of trial
+trlbegin = sample + pretrig;      %  begin trial
+trlend   = sample + posttrig;     %  end of trial
 offset = pretrig*ones(size(trlend));              %  a negative offset indicates that the trial begins before the trigger      
 
 cfg.trl = [trlbegin trlend offset]; % concatenate the columns into the trl matrix
   
   
- trdat.alerting_no_cue = ft_redefinetrial(cfg,data);
+ trdat = ft_redefinetrial(cfg,data);
  
  %make trl matrix double cue
-cfg.trl = []
-trlbegin = sample_d + pretrig;      %  begin trial
-trlend   = sample_d + posttrig;     %  end of trial
-offset = pretrig*ones(size(trlend));              %  a negative offset indicates that the trial begins before the trigger      
 
-cfg.trl = [trlbegin trlend offset]; % concatenate the columns into the trl matrix
-  
-  
- trdat.alerting_double_cue = ft_redefinetrial(cfg,data);
- 
- info = cfg.event(value) %get trial info for both conditions
 
    % trdat.trialinfo  =  {info(:).value}';
-     trdat.alerting_no_cue.trialinfo  =  value_marker_no_cue
-     trdat.alerting_double_cue.trialinfo  =  value_marker_double
+     trdat.trialinfo  =  value_marker
+
    % trdat.trialinfo_markers  =  trdat.trialinfo
    % trdat.trialinfo  =  str2double(trdat.trialinfo) %convert trial info to NaN double for auto 
     save([wpms.dirs.CWD wpms.dirs.preproc wpms.names{name_i} '_EOGCORR_trdat'],'trdat','-v7.3'); % added this into to save trial data for auto artifact rejeciton 
